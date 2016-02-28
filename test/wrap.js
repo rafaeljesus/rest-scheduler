b9faf51ccf50c5155727cef1778652efd3bd8f48
rafaeljesus <rafaelljesus86@gmail.com>
2016-02-28 20:08:27 -0300

    Remove failling coverage

--- test/wrap.js
+++ test/wrap.js
@@ -1,7 +1,7 @@
-export default (fn) => {
+export default function (fn) {
   return async function (done) {
     try {
-      done(null, await fn())
+      done(null, await fn.apply(this, arguments))
     } catch (err) {
       done(err)
     }
